89048f0a97d69f3def4c995d71af2c81d21df1ee
Soliman Cifuentes <soliman.cifuentes@icloud.com>
2023-08-13 21:27:33 -0600

    Created Gradebook Python 3 Project

---
+++ PYTHON 3/Gradebook.py
@@ -0,0 +1,34 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+# Your code below: 
+
+#Challenge 1
+subjects = ["physics", "calculus", "poetry", "history"]
+
+#Challenge 2
+grades = ["98", "97", "85", "88"]
+
+#Challenge 3
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+#Challenge 4
+gradebook.append(["computer science", 100])
+
+#Challenge 5
+gradebook.append(["visual arts", 93])
+
+#Challenge 6
+gradebook[5][1] = 98
+
+#Challenge 7
+gradebook[2].remove(85)
+
+#Challenge 8
+gradebook[2].append("Pass")
+
+#Challenge 9
+full_gradebook = last_semester_gradebook + gradebook
+
+#Challenge 10
+print(full_gradebook)
+
