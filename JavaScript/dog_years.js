6ff1c405bdaadf82d2787f43b442e106cd944818
Soliman Cifuentes <soliman.cifuentes@icloud.com>
2024-06-11 00:16:47 -0600

    Added dog years project

---
+++ JavaScript/dog_years.js
@@ -0,0 +1,24 @@
+// saving my age
+const myAge = 31;
+
+// setting early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// setting later years value
+let laterYears = myAge - 2;
+
+// setting later years in dog years
+laterYears *= 4;
+
+// console.log(earlyYears);
+// console.log(laterYears);
+
+// saving your age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+// saving my name in all lowercase.
+myName = 'Soliman'.toLowerCase();
+
+// printing the result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
